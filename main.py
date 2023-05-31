9d7c9358b6247451f301f7547143b6a0da61d835
Imalsha0330 <samaranayake.imalsha@gmail.com>
2023-05-31 17:02:36 +0200

    Gave each option a number

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 print("Welcome To The Liquor Store")
-"""The main Heading added. 
-"""
+#The main heading
+
+print("Please Select The Number For Your Preference")
+
+print("Press Number 1 for Hard Liquor")
+print("Press Number 2 for Red Wine")
+print("Press Number 3 for White Wine")
+print("Press Number 4 for Beer")
+#Giving the option to select the prefered number
