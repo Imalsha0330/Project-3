eea0ee89c7a0f43a5d879d1c70a94c35d2a57044
Imalsha0330 <samaranayake.imalsha@gmail.com>
2023-05-31 17:03:58 +0200

    Input added to Enter the Number

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ print("Press Number 2 for Red Wine")
 print("Press Number 3 for White Wine")
 print("Press Number 4 for Beer")
 #Giving the option to select the prefered number
+
+num = int(input("Please Enter a Number:"))
+result = None
+#input added to enter the number
