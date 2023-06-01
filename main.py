cc6392704750020c1a14b46019683761f25688bc
Imalsha0330 <samaranayake.imalsha@gmail.com>
2023-06-01 10:55:14 +0200

    The nested if statement added under beer

--- main.py
+++ main.py
@@ -220,6 +220,24 @@ elif num is 4:#The first Elif Statement for Beer
     print("Press 3 For Corona")
     num1 = int(input("Please Enter a Number:"))#Input bar to Enter the Number
     result = None
+    if num1 == 1:#The nested if statement under Beer
+        print("You Have Chosen Carlsberg 500ml Bottel")#option what have chosen
+        print("Please Select The Quantity")
+        quantityhl = int(input("Please Enter Your Quantity:"))#Input bar to enter the quantity
+        quantityhl1 = (quantityhl * 10)#multiplying the quantity with the price
+        print("Your Total will be")
+        print(quantityhl1)#getting the multiplied value
+        print("All The Rates Are In US$")
+        namehl = str(input("Please Enter Your Name:"))#getting the name
+        phhl = str(input ("Please Enter Your Phone Number:"))#getting the phone number
+        adhl = str(input("Please Enter Your Address:"))#getting the address
+        print("Thank You For Your Order" + " " + namehl)
+        print("One Of Our Delivery Agent Will Contact You," + " " + phhl + " " + "On This Number")
+        print("And Once We Got an Order Confirmation From Your End, We Will Deliver Your Oder to" + " " + adhl)#confirmation message
+        idhl = str(input("Please Enter Your ID Number For Age Verification"))#Id Entering Input bar for age verification
+        print("Your ID number" + " " + idhl + " " + "Is Under Age Verification Process")#Message about age verification
+        print("We Will Contact You Within 30 Mins After The Age Verification")
+        print("Thank you for your oder!")#Final Thank you message.
 else:#Else Statement if Entered a wrong value
     print("Invalid Number")
     print(num)
