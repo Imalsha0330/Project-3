52a6c5511066bc54f295bd2b64400ca83ba8b99c
Imalsha0330 <samaranayake.imalsha@gmail.com>
2023-06-01 10:39:20 +0200

    The Nested else statement added under white wine

--- main.py
+++ main.py
@@ -208,6 +208,9 @@ elif num is 3:#The first Elif Statement for White Wine
         print("Your ID number" + " " + idhl + " " + "Is Under Age Verification Process")#Message about age verification
         print("We Will Contact You Within 30 Mins After The Age Verification")
         print("Thank you for your oder!")#Final Thank you message.
+    else:#The Else Statement under main second elif statement
+        print("Invalid Number")#If the Number Invalid this message will appear
+        print(num1)  
 elif num is 4:#The first Elif Statement for Beer
     print("You Have Chosen Beer")
     print("Below Are Our Options:")
